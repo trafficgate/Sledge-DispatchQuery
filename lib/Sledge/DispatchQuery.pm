1be538ec2113a65133f8b085cd2cf1f43a936730
miyagawa <>
2004-02-24 08:31:09 +0000

    added POD document

--- lib/Sledge/DispatchQuery.pm
+++ lib/Sledge/DispatchQuery.pm
@@ -20,3 +20,48 @@ sub dispatch_query {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Sledge::DispatchQuery - Mix-in to add dispatch_query to your Pages class
+
+=head1 SYNOPSIS
+
+  package Proj::Pages;
+  use Sledge::DispatchQuery;
+
+  # in blahblah/index.cgi
+  use Proj::Pages::Blahblah;
+  Proj::Pages::Blahblah->dispatch_query(".action");
+
+=head1 DESCRIPTION
+
+Sledge::DispatchQuery is a mix-in to add C<dispatch_query> to your
+Pages class. This method allows you to create only C<index.cgi> in
+each directory and call various page via CGI query paramter.
+
+In the example in SYNOPSIS,
+
+  http://example.com/blahblah/?.action=edit
+
+would make a call for
+
+  Project::Pages::Blahblah->new->dispatch('edit');
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Sledge.
+
+=head1 SEE ALSO
+
+L<Sledge>
+
+=cut
+
